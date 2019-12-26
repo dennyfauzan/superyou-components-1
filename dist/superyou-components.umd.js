53f0ff94c922ac083779bf7c1dfb2e583b2fe87e
dennyfauzan <dennyfauzan92@gmail.com>
2019-12-26 11:40:06 +0700

    update and build prod

--- dist/superyou-components.umd.js
+++ dist/superyou-components.umd.js
@@ -293,6 +293,17 @@ $({ target: 'String', proto: true, forced: !correctIsRegExpLogic('includes') }, 
 
 /***/ }),
 
+/***/ "35d1":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var _node_modules_mini_css_extract_plugin_dist_loader_js_ref_8_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_8_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_8_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_8_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_InvoiceDetail_vue_vue_type_style_index_0_id_58247cdc_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("6c02");
+/* harmony import */ var _node_modules_mini_css_extract_plugin_dist_loader_js_ref_8_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_8_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_8_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_8_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_InvoiceDetail_vue_vue_type_style_index_0_id_58247cdc_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_mini_css_extract_plugin_dist_loader_js_ref_8_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_8_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_8_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_8_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_InvoiceDetail_vue_vue_type_style_index_0_id_58247cdc_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__);
+/* unused harmony reexport * */
+ /* unused harmony default export */ var _unused_webpack_default_export = (_node_modules_mini_css_extract_plugin_dist_loader_js_ref_8_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_8_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_8_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_8_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_InvoiceDetail_vue_vue_type_style_index_0_id_58247cdc_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0___default.a); 
+
+/***/ }),
+
 /***/ "37e8":
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -400,17 +411,6 @@ module.exports = function (it) {
 
 /***/ }),
 
-/***/ "4774":
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-/* harmony import */ var _node_modules_mini_css_extract_plugin_dist_loader_js_ref_8_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_8_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_8_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_8_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_InvoiceDetail_vue_vue_type_style_index_0_id_7d068cb8_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("9c86");
-/* harmony import */ var _node_modules_mini_css_extract_plugin_dist_loader_js_ref_8_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_8_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_8_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_8_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_InvoiceDetail_vue_vue_type_style_index_0_id_7d068cb8_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_mini_css_extract_plugin_dist_loader_js_ref_8_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_8_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_8_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_8_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_InvoiceDetail_vue_vue_type_style_index_0_id_7d068cb8_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__);
-/* unused harmony reexport * */
- /* unused harmony default export */ var _unused_webpack_default_export = (_node_modules_mini_css_extract_plugin_dist_loader_js_ref_8_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_8_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_8_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_8_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_InvoiceDetail_vue_vue_type_style_index_0_id_7d068cb8_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0___default.a); 
-
-/***/ }),
-
 /***/ "4930":
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -422,6 +422,13 @@ module.exports = !!Object.getOwnPropertySymbols && !fails(function () {
   return !String(Symbol());
 });
 
+
+/***/ }),
+
+/***/ "4d10":
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
 
 /***/ }),
 
@@ -642,6 +649,13 @@ module.exports = {
   getterFor: getterFor
 };
 
+
+/***/ }),
+
+/***/ "6c02":
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
 
 /***/ }),
 
@@ -869,6 +883,17 @@ module.exports = function (it) {
 
 /***/ }),
 
+/***/ "8fb8":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var _node_modules_mini_css_extract_plugin_dist_loader_js_ref_8_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_8_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_8_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_8_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_CardInvoice_vue_vue_type_style_index_0_id_6f9be3d3_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("4d10");
+/* harmony import */ var _node_modules_mini_css_extract_plugin_dist_loader_js_ref_8_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_8_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_8_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_8_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_CardInvoice_vue_vue_type_style_index_0_id_6f9be3d3_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_mini_css_extract_plugin_dist_loader_js_ref_8_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_8_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_8_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_8_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_CardInvoice_vue_vue_type_style_index_0_id_6f9be3d3_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__);
+/* unused harmony reexport * */
+ /* unused harmony default export */ var _unused_webpack_default_export = (_node_modules_mini_css_extract_plugin_dist_loader_js_ref_8_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_8_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_8_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_8_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_CardInvoice_vue_vue_type_style_index_0_id_6f9be3d3_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0___default.a); 
+
+/***/ }),
+
 /***/ "90e3":
 /***/ (function(module, exports) {
 
@@ -951,13 +976,6 @@ exports.f = DESCRIPTORS ? nativeDefineProperty : function defineProperty(O, P, A
   return O;
 };
 
-
-/***/ }),
-
-/***/ "9c86":
-/***/ (function(module, exports, __webpack_require__) {
-
-// extracted by mini-css-extract-plugin
 
 /***/ }),
 
@@ -1430,12 +1448,12 @@ if (typeof window !== 'undefined') {
 // Indicate to webpack that this file can be concatenated
 /* harmony default export */ var setPublicPath = (null);
 
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"cc4b0e26-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/card-invoice/CardInvoice.vue?vue&type=template&id=6316fb33&
-var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('base-card',{attrs:{"header-style":_vm.cardStyles.header}},[_c('h3',{attrs:{"slot":"card-header"},slot:"card-header"},[_vm._v(_vm._s(_vm.title))]),_c('div',{attrs:{"slot":"card-body"},slot:"card-body"},[_c('InvoiceBody',{attrs:{"details":_vm.detailPayment}},[_c('baseChip',{attrs:{"slot":"payment-status","type":_vm.detailPayment.status},slot:"payment-status"},[_c('span',{staticClass:"text"},[_vm._v(_vm._s(_vm.detailPayment.status_message))])]),_c('baseButton',{attrs:{"slot":"payment-button","btn-text":"BAYAR SEKARANG","isDisabled":_vm.isActionDisabled},on:{"onClick":_vm.handleClickCTA},slot:"payment-button"}),_c('div',{staticClass:"payment-list",attrs:{"slot":"payment-detail"},slot:"payment-detail"},_vm._l((_vm.detailPayment.invoices),function(invoice){return _c('InvoiceDetail',{key:invoice.id,attrs:{"datas":invoice}})}),1)],1)],1)])}
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"cc4b0e26-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/card-invoice/CardInvoice.vue?vue&type=template&id=6f9be3d3&scoped=true&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('base-card',{attrs:{"header-style":_vm.cardStyles.header}},[_c('h3',{attrs:{"slot":"card-header"},slot:"card-header"},[_vm._v(_vm._s(_vm.title))]),_c('div',{attrs:{"slot":"card-body"},slot:"card-body"},[_c('InvoiceBody',{attrs:{"details":_vm.detailPayment}},[_c('baseChip',{attrs:{"slot":"payment-status","type":_vm.detailPayment.status},slot:"payment-status"},[_c('span',{staticClass:"text"},[_vm._v(_vm._s(_vm.detailPayment.status_message))])]),_c('baseButton',{attrs:{"slot":"payment-button","btn-text":"BAYAR SEKARANG","isDisabled":_vm.isActionDisabled},on:{"onClick":_vm.handleClickCTA},slot:"payment-button"}),_c('div',{staticClass:"payment-list",attrs:{"slot":"payment-detail"},slot:"payment-detail"},[_vm._l((_vm.detailPayment.invoices),function(invoice){return _c('InvoiceDetail',{key:invoice.id,attrs:{"datas":invoice}})}),_c('div',{staticClass:"invoice-summary"},[_c('div',{staticClass:"invoice-summary__wrapper"},[_c('h2',[_vm._v("Payment Fee")]),_c('span',[_vm._v(_vm._s(_vm.detailPayment.fee))])])])],2)],1)],1)])}
 var staticRenderFns = []
 
 
-// CONCATENATED MODULE: ./src/components/card-invoice/CardInvoice.vue?vue&type=template&id=6316fb33&
+// CONCATENATED MODULE: ./src/components/card-invoice/CardInvoice.vue?vue&type=template&id=6f9be3d3&scoped=true&
 
 // CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"cc4b0e26-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/atoms/BaseCard.vue?vue&type=template&id=2bfbd4af&scoped=true&
 var BaseCardvue_type_template_id_2bfbd4af_scoped_true_render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"su_card"},[_c('div',{staticClass:"su_card-header",style:([_vm.headerStyle])},[_vm._t("card-header",[_c('h3',[_vm._v("Card Header")])])],2),_c('div',{staticClass:"su_card-body",style:([_vm.bodyStyle])},[_vm._t("card-body",[_vm._v("card body")])],2)])}
@@ -1900,12 +1918,12 @@ var InvoiceBody_component = normalizeComponent(
 )
 
 /* harmony default export */ var InvoiceBody = (InvoiceBody_component.exports);
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"cc4b0e26-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/molecules/invoice/InvoiceDetail.vue?vue&type=template&id=7d068cb8&scoped=true&
-var InvoiceDetailvue_type_template_id_7d068cb8_scoped_true_render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{attrs:{"id":"invoice-detail"}},[_c('div',{staticClass:"detail-left"},[_c('div',{staticClass:"product-icon",staticStyle:{"width":"70px"}},[_c('BaseIconProductAndPlan',{attrs:{"product":_vm.getIconProduct,"plan":_vm.getBadgeType}})],1),_c('div',{staticClass:"product-basic-rider-name"},[_c('ProductCompletedName',{attrs:{"details":_vm.allProductNameAndPolicyNumber}})],1),_c('a',{staticClass:"see-product-detail",attrs:{"href":("/dashboard/e-policy/" + (_vm.datas.productId))}},[_vm._v("Detail Produk")])]),_c('div',{staticClass:"detail-right"},[_c('span',{staticClass:"price"},[_c('span',[_vm._v(_vm._s(_vm.datas.price))])]),(_vm.datas.riders && _vm.datas.riders.length)?_vm._l((_vm.datas.riders),function(riderPrice){return _c('span',{key:riderPrice.price,staticClass:"price"},[_c('span',[_vm._v(_vm._s(riderPrice.price))])])}):_vm._e()],2)])}
-var InvoiceDetailvue_type_template_id_7d068cb8_scoped_true_staticRenderFns = []
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"cc4b0e26-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/molecules/invoice/InvoiceDetail.vue?vue&type=template&id=58247cdc&scoped=true&
+var InvoiceDetailvue_type_template_id_58247cdc_scoped_true_render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"invoice-detail"},[_c('div',{staticClass:"detail-left"},[_c('div',{staticClass:"product-icon",staticStyle:{"width":"70px"}},[_c('BaseIconProductAndPlan',{attrs:{"product":_vm.getIconProduct,"plan":_vm.getBadgeType}})],1),_c('div',{staticClass:"product-basic-rider-name"},[_c('ProductCompletedName',{attrs:{"details":_vm.allProductNameAndPolicyNumber}})],1),_c('a',{staticClass:"see-product-detail",attrs:{"href":("/dashboard/e-policy/" + (_vm.datas.productId))}},[_vm._v("Detail Produk")])]),_c('div',{staticClass:"detail-right"},[_c('span',{staticClass:"price"},[_c('span',[_vm._v(_vm._s(_vm.datas.price))])]),(_vm.datas.riders && _vm.datas.riders.length)?_vm._l((_vm.datas.riders),function(riderPrice){return _c('span',{key:riderPrice.price,staticClass:"price"},[_c('span',[_vm._v(_vm._s(riderPrice.price))])])}):_vm._e()],2)])}
+var InvoiceDetailvue_type_template_id_58247cdc_scoped_true_staticRenderFns = []
 
 
-// CONCATENATED MODULE: ./src/molecules/invoice/InvoiceDetail.vue?vue&type=template&id=7d068cb8&scoped=true&
+// CONCATENATED MODULE: ./src/molecules/invoice/InvoiceDetail.vue?vue&type=template&id=58247cdc&scoped=true&
 
 // EXTERNAL MODULE: ./node_modules/core-js/modules/es.array.includes.js
 var es_array_includes = __webpack_require__("caad");
@@ -2146,8 +2164,8 @@ var BaseIconProductAndPlan_component = normalizeComponent(
 });
 // CONCATENATED MODULE: ./src/molecules/invoice/InvoiceDetail.vue?vue&type=script&lang=js&
  /* harmony default export */ var invoice_InvoiceDetailvue_type_script_lang_js_ = (InvoiceDetailvue_type_script_lang_js_); 
-// EXTERNAL MODULE: ./src/molecules/invoice/InvoiceDetail.vue?vue&type=style&index=0&id=7d068cb8&lang=scss&scoped=true&
-var InvoiceDetailvue_type_style_index_0_id_7d068cb8_lang_scss_scoped_true_ = __webpack_require__("4774");
+// EXTERNAL MODULE: ./src/molecules/invoice/InvoiceDetail.vue?vue&type=style&index=0&id=58247cdc&lang=scss&scoped=true&
+var InvoiceDetailvue_type_style_index_0_id_58247cdc_lang_scss_scoped_true_ = __webpack_require__("35d1");
 
 // CONCATENATED MODULE: ./src/molecules/invoice/InvoiceDetail.vue
 
@@ -2160,17 +2178,23 @@ var InvoiceDetailvue_type_style_index_0_id_7d068cb8_lang_scss_scoped_true_ = __w
 
 var InvoiceDetail_component = normalizeComponent(
   invoice_InvoiceDetailvue_type_script_lang_js_,
-  InvoiceDetailvue_type_template_id_7d068cb8_scoped_true_render,
-  InvoiceDetailvue_type_template_id_7d068cb8_scoped_true_staticRenderFns,
+  InvoiceDetailvue_type_template_id_58247cdc_scoped_true_render,
+  InvoiceDetailvue_type_template_id_58247cdc_scoped_true_staticRenderFns,
   false,
   null,
-  "7d068cb8",
+  "58247cdc",
   null
   
 )
 
 /* harmony default export */ var InvoiceDetail = (InvoiceDetail_component.exports);
 // CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js??ref--12-0!./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/card-invoice/CardInvoice.vue?vue&type=script&lang=js&
+//
+//
+//
+//
+//
+//
 //
 //
 //
@@ -2249,7 +2273,11 @@ var InvoiceDetail_component = normalizeComponent(
 });
 // CONCATENATED MODULE: ./src/components/card-invoice/CardInvoice.vue?vue&type=script&lang=js&
  /* harmony default export */ var card_invoice_CardInvoicevue_type_script_lang_js_ = (CardInvoicevue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./src/components/card-invoice/CardInvoice.vue?vue&type=style&index=0&id=6f9be3d3&lang=scss&scoped=true&
+var CardInvoicevue_type_style_index_0_id_6f9be3d3_lang_scss_scoped_true_ = __webpack_require__("8fb8");
+
 // CONCATENATED MODULE: ./src/components/card-invoice/CardInvoice.vue
+
 
 
 
@@ -2263,7 +2291,7 @@ var CardInvoice_component = normalizeComponent(
   staticRenderFns,
   false,
   null,
-  null,
+  "6f9be3d3",
   null
   
 )
