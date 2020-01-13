91d1416bbfd81a27b8f2bbc25618250579389f59
dennyfauzan <dennyfauzan92@gmail.com>
2020-01-13 16:23:16 +0700

    :hammer: baseSelect

---
+++ src/atoms/BaseSelect.vue
@@ -0,0 +1,94 @@
+<template>
+  <div id="su-base-select">
+    <label>Select Option</label>
+    <v-select
+      class="base-select"
+      :options="options"
+      :selectable="option => option.val !== 'INA'"
+      @input="onSelectOption"
+    ></v-select>
+  </div>
+</template>
+
+<script>
+import vSelect from "vue-select";
+
+export default {
+  name: "BaseSelect",
+  data() {
+    return {
+      selectedData: null
+    };
+  },
+  components: {
+    vSelect
+  },
+  props: {
+    options: {
+      type: Array,
+      default: function() {
+        return [
+          { label: "Indonesia", val: "INA" },
+          { label: "Canada", val: "CA" },
+          { label: "Australia", val: "AUS" }
+        ];
+      }
+    }
+  },
+  methods: {
+    onSelectOption(selectedData) {
+      console.log(selectedData);
+      this.selectedData = selectedData.val;
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+#su-base-select {
+  label {
+    color: #708697;
+    font-size: 12px;
+  }
+  .base-select {
+    .vs__dropdown-toggle {
+      border: none;
+      border-radius: 0;
+      border-bottom: 1px solid rgba(0, 0, 0, 0.3);
+
+      .vs__selected-options {
+        padding-left: 0;
+        .vs__selected {
+          padding-left: 0;
+          margin: 10px 2px 4px 0;
+          font-size: 14px;
+        }
+        .vs__search {
+          font-size: 14px;
+          color: #0d294a;
+          padding-bottom: 4px;
+        }
+      }
+
+      .vs__actions {
+        .vs__open-indicator {
+          fill: #0d294a;
+          transform: scale(0.85);
+        }
+      }
+    }
+    .vs__dropdown-menu {
+      .vs__dropdown-option {
+        font-size: 14px;
+        &--highlight {
+          background-color: #00aaae;
+        }
+      }
+      .vs__no-options {
+        font-size: 14px;
+        color: #708697;
+      }
+    }
+  }
+}
+</style>
