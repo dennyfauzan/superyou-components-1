f5f83dd410182e90b39a0c33070ce0c95bd74121
dennyfauzan <dennyfauzan92@gmail.com>
2020-01-28 10:36:12 +0700

    register base components form

--- src/_build.js
+++ src/_build.js
@@ -1,4 +1,14 @@
 // Components
 import _CardInvoice from "@/components/card-invoice/CardInvoice.vue";
+import _BaseInput from "@/components/atoms/BaseInput.vue";
+import _BaseCheckboxAndRadio from "@/components/atoms/BaseCheckboxAndRadio.vue";
+import _BaseInputDate from "@/components/atoms/BaseInputDate.vue";
+import _BaseSelect from "@/components/atoms/BaseSelect.vue";
+import _BaseStepper from "@/components/atoms/BaseStepper.vue";
 
 export const CardInvoice = _CardInvoice;
+export const BaseInput = _BaseInput;
+export const BaseCheckboxAndRadio = _BaseCheckboxAndRadio;
+export const BaseInputDate = _BaseInputDate;
+export const BaseSelect = _BaseSelect;
+export const BaseStepper = _BaseStepper;
