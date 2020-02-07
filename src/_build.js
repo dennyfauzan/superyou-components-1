717abe6a3c55474cfe068340cbe89496d0eaee36
dennyfauzan <dennyfauzan92@gmail.com>
2020-02-07 11:01:29 +0700

    register base modal and invoice detail

--- src/_build.js
+++ src/_build.js
@@ -5,6 +5,8 @@ import _BaseCheckboxAndRadio from "@/atoms/BaseCheckboxAndRadio.vue";
 import _BaseInputDate from "@/atoms/BaseInputDate.vue";
 import _BaseSelect from "@/atoms/BaseSelect.vue";
 import _BaseStepper from "@/atoms/BaseStepper.vue";
+import _BaseModal from "@/atoms/BaseModal.vue";
+import _InvoiceDetails from "@/molecules/invoice/InvoiceDetails.vue";
 
 // Style
 import "vue-select/src/scss/vue-select.scss";
@@ -15,3 +17,5 @@ export const BaseCheckboxAndRadio = _BaseCheckboxAndRadio;
 export const BaseInputDate = _BaseInputDate;
 export const BaseSelect = _BaseSelect;
 export const BaseStepper = _BaseStepper;
+export const InvoiceDetails = _InvoiceDetails;
+export const BaseModal = _BaseModal;
